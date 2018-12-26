b5c011762b4a553248e70624cf0b2dc9067d214f
Saif Ijaz <41892175+saif86@users.noreply.github.com>
2018-12-26 18:13:58 +0800

    Update Task3_Inheritance_Aggregation.cpp

--- Task3_Inheritance_Aggregation.cpp
+++ Task3_Inheritance_Aggregation.cpp
@@ -1,15 +1,39 @@
-#include <iostream> 
-#include "Company.h" 
-#include "Air_Transport_t3.h" 
-#include "Land_Transport_t3.h" 
-using namespace std; 
+/**
+* @file Task3_Inheritance_Aggregation.cpp
+*
+* @brief This code implements Inheritance & Aggregation concepts.
+* Land & Air Transport classes are inhereted from transport. 
+* Class Company has aggregation with Room and Hall Classes.
+*
+* @author Saif Ullah Ijaz
+*
+*/
 
-void main() { 
-	Company c1; 
-	Air_Transport air_T1; 
-	Land_Transport land_T2; 
+#include <iostream> 
+using namespace std;
+#include "Company.h" 
+#include "AirTransportT3.h" 
+#include "LandTransportT3.h" 
+
+
+// function main begins program execution
+void main()
+{
+	Room r[2];
+	r[0].SetRoom(2, 2);
+	r[1].SetRoom(3, 4);
 	
-	cout << "Welcome to PakFood Internationals" << endl; 
 	
-	system("pause"); 
+	Company c1;
+	c1.SetRoom(&r[0]);
+	c1.SetRoom(&r[1]);
+	c1.PrintRooms();
+	
+	AirTransport air_T1;
+	LandTransport land_T2;
+
+	cout << "Welcome to PakFood Internationals" << endl;
+	
+	system("pause");
 }
+// end main
